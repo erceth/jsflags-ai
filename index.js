c1d7bb67c9e78bbd2a8fd3ba41bda7b8adc4fa99
erceth <erceth@gmail.com>
2015-03-10 17:46:00 -0600

    added proper node modules

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+ var socket = require('socket.io-client')('http://localhost');
+ socket.on('connect', function (data) {
+ 	console.log(data);
+ });
 
-var port = process.argv[2];
-
-
-
+ 
