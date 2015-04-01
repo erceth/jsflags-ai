0be7c21f13739aaad036aa08873c1164ee3905db
erceth <erceth@gmail.com>
2015-03-31 23:50:46 -0600

    stable veriosn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ socket.on("init", function(initD) {
 	if (connected) {
 		return false;
 	}
+	socket.on("disconnect", function() {
+		process.exit(1);
+	});
 	connected = true;
 	initData = initD;
 	selectedPlayer = initData.players[playerSelection];
@@ -66,7 +69,7 @@ function startInterval() {
 
 	setInterval(function() {
 		fire();
-	}, 3000);
+	}, 5000);
 }
 
 
@@ -130,33 +133,63 @@ function calculateGoal() {
 
 
 		//set speed
-		if (distance >= 100) {
+		if (distance >= 20) {
 			myTanks[i].goal.speed = 1;
 		} else {
-			myTanks[i].goal.speed = 0;
+			//myTanks[i].goal.speed = 0;
 			myTanks[i].missionAccomplished();
 		}
 	}
 
 }
 
-function calculateObstacle(obstacles) {
-	var distance, relativeX, relativeY, angle, degrees;
+// function calculateObstacle(obstacles) {
+// 	var distance, relativeX, relativeY, angle, degrees, obstacleRadius, bufferZone = 10;
 
-	console.log(obstacles);
+// 	console.log(obstacles);
 
-	for (var i = 0, maxi = myTanks.length; i < maxi; i++) {
-		for (var j = 0, maxj = obstacles.length; j < maxj; j++) {
-			distance = round(Math.sqrt(Math.pow(( obstacles - myTanks[i].position.x ), 2) + Math.pow(( goal.y - myTanks[i].position.y ), 2)), 4);
-			relativeX = goal.x - myTanks[i].position.x; //relative
-			relativeY = goal.y - myTanks[i].position.y;
-			angle = round(Math.atan2(-(relativeY), relativeX), 4);
+// 	for (var i = 0, maxi = myTanks.length; i < maxi; i++) {
+// 		for (var j = 0, maxj = obstacles.length; j < maxj; j++) {
+// 			distance = round(Math.sqrt(Math.pow( obstacles[j].center.x  - myTanks[i].center.x ), 2) + Math.pow(( obstacles[j].center.y - myTanks[i].center.y ), 2)), 4);
+// 			if (distance < (obstacles[j].radius + bufferZone) ) {
+// 				var pointNearestTank = findClosestPointOnCircle(myTanks[i].center, obstacles[j].center, obstacles[j].radius);
+// 				var pointNearestGoal = findClosestPointOnCircle(myTanks[i].target, obstacles[j].center, obstacles[j].radius);
+// 				distanceBetweenTwoPointsOnACircle(pointNearestTank, pointNearestGoal, obstacles[j].center, obstacles[j].radius);
+// 			}
+// 			relativeX = goal.x - myTanks[i].position.x; //relative
+// 			relativeY = goal.y - myTanks[i].position.y;
+// 			angle = round(Math.atan2(-(relativeY), relativeX), 4);
 
 
-		}
-	}
+// 		}
+// 	}
 
-}
+// 	function findClosestPointOnCircle(otherPoint, circleCenter, circleRadius) {
+// 		var vX = otherPoint.x - circleCenter.x;
+// 		var vY = otherPoint.y - circleCenter.y;
+// 		var magV = sqrt((vX * vX) + (vY * vY));
+// 		return {
+// 			x: otherPoint.x + vX / magV * circleRadius,
+// 			y: otherPoint.y + vY / magV * circleRadius
+// 		};
+// 	}
+// 	//return + shortest route is clockwise or - shortest route is counter clockwise
+// 	function distanceBetweenTwoPointsOnACircle(point1, point2, cicleCenter radius) {
+// 		var degreeOfPoint1 = (Math.atan2(point1.y - cicleCenter.y, point1.x - cicleCenter.x) * 180/Math.PI + 360) % 360;
+// 		var degreeOfPoint2 = (Math.atan2(point2.y - cicleCenter.y, point2.x - cicleCenter.x) * 180/Math.PI + 360) % 360;
+// 		var difference = degreeOfPoint1 - degreeOfPoint2;
+// 		if ((difference) > 0 && difference < 180) {
+// 			return -1;
+// 		} else if (difference > 0 && difference > 180) {
+// 			return 1;
+// 		} else if (difference < 0 $$ difference < -180) {
+// 			return -1;
+// 		} else {
+// 			return 1;
+// 		}
+// 	}
+
+// }
 
 function sendBackCommands() {
 	//add up all calculations
@@ -184,6 +217,10 @@ var Tank = function(tankNumber) {
 		speed: 0,
 		angleVel: 0
 	};
+	this.avoidObstacle = {
+		speed: 0,
+		angleVel: 0
+	};
 	this.target = {x: 100, y: 100};
 	this.hasATarget = false;
 };
@@ -197,6 +234,7 @@ Tank.prototype = {
 	},
 	generateTarget: function() {
 		var randomNumber = Math.floor(Math.random() * 10 % enemyBases.length); //random num between 0 and enemyBases.length
+		//var randomNumber = 1;
 		//console.log(randomNumber, enemyBases[randomNumber].base);
 		this.target = enemyBases[randomNumber].base.position;
 		this.hasATarget = true;
